b5d47533047dde2389d974604c1bed183df15db7
Brayden Robbins <braydenrobbins98@gmail.com>
2019-05-04 08:59:12 -0500

    fixed the delete all bug

--- routes/class.js
+++ routes/class.js
@@ -27,15 +27,18 @@ router.post('/', function (req, res, next) {
     });
 });
 
-router.delete('/', function (req, res, next) {
-    classes.remove((err, clas) => {
-        if (err) {
-            console.error("couldnt get class", err)
-            res.send('couldnt get class');
-        } else {
-            res.json(clas)
-        }
-    })
+router.delete('/:id', function (req, res, next) {
+    console.log(req.params.id);
+    const course = classes.findByIdAndDelete(req.params.id)
+        .then(course => res.sendStatus(204));
+    // classes.remove((err, clas) => {
+    //     if (err) {
+    //         console.error("couldnt get class", err)
+    //         res.send('couldnt get class');
+    //     } else {
+    //         res.json(clas)
+    //     }
+    // })
 });
 
 module.exports = router;
