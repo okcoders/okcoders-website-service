1bb0c729697a6cfee80a401185446e11980c90e5
Joseph Okpara <joejr.okpara@gmail.com>
2019-06-15 10:56:41 -0500

    added middleware and began protecting alumni route

---
+++ middleware/middleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+ const token = req.header('x-auth-token');
+ if (!token) {
+   return res.sendStatus(401);
+ }
+
+ try {
+   const decoded = jwt.verify(token, process.env.AUTH_SECRET);
+   req.user = decoded.user;
+   next();
+ } catch (err) {
+   res.sendStatus(401);
+ }
+};
